66d2bc589f3b2d09fec25b7534c8ae7b27352252
Amit Vyas <23amitvyas@gmail.com>
2022-12-25 22:46:07 +0300

    adding API for update and delete

--- src/main/java/com/vyas/app/rest/Controller/api.java
+++ src/main/java/com/vyas/app/rest/Controller/api.java
@@ -2,9 +2,12 @@ package com.vyas.app.rest.Controller;
 import org.hibernate.usertype.UserCollectionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +36,26 @@ public class api {
         uR.save(user);
         return "Saved!!";
 
+    }
+
+    @PutMapping(value = "/update/{uid}")
+    public String updateOperation(@PathVariable long uid, @RequestBody users user){
+        users updateuser = uR.findById(uid).get();
+        updateuser.setfname(user.getfname());
+        updateuser.setlname(user.getlname());
+        updateuser.setAge(user.getAge());
+        updateuser.setPreference(user.getPreference());
+        updateuser.setBudget(user.getBudget());
+        uR.save(updateuser);
+        return "Update done!!";
+
+    }
+
+    @DeleteMapping(value = "/delete/{uid}")
+    public String deletUser(@PathVariable long uid){
+        users deleteUser = uR.findById(uid).get();
+        uR.delete(deleteUser);
+        return "user deleted with id: "+ uid;
     }
 
 }
